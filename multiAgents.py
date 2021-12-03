cb2a1fe4735d7436af9b4061e0d467a654507796
Fady Kittan <43303441+fadykittan@users.noreply.github.com>
2021-12-03 22:26:05 +0200

    q2 - 5 of 5

--- multiAgents.py
+++ multiAgents.py
@@ -162,7 +162,32 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        def findValue(state, depth):
+            if state.isWin() or state.isLose():
+                return self.evaluationFunction(state)
+            if depth == self.depth*state.getNumAgents():
+                return self.evaluationFunction(state)
+            if depth % state.getNumAgents() == 0:
+                return maxValue(state, depth, depth % state.getNumAgents())
+            else:
+                return minValue(state, depth, depth % state.getNumAgents())
+
+        def maxValue(state, depth, agentIndex):
+            return max(list(map(lambda x: findValue(state.generateSuccessor(agentIndex, x), depth+1), state.getLegalActions(agentIndex))))
+
+        def minValue(state, depth, agentIndex):
+            return min(list(map(lambda x: findValue(state.generateSuccessor(agentIndex, x), depth+1), state.getLegalActions(agentIndex))))
+
+        maxV = -float('inf')
+        minimaxAction = 0
+        if gameState.isWin() or gameState.isLose():
+            return self.evaluationFunction(gameState)
+        for action in gameState.getLegalActions(0):
+            value = findValue(gameState.generateSuccessor(0, action), 1)
+            if value > maxV:
+                minimaxAction = action
+                maxV = value
+        return minimaxAction
 
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
